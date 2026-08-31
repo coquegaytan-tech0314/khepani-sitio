8298a53f4d50b1bdcf88859027d179747f72b29b
coquegaytan-tech0314 <coquegaytan@gmail.com>
2026-08-31 13:05:10 -0600

    Restore full Sellos proposal HTML (overwrite gzip loader)
    Restore the published Sellos HTML from 776e87e and remove gzip-loader leftovers.

--- propuestas/sellos/page.js
+++
@@ -1,6 +0,0 @@
-(async function () {
-  var b64 = window.__KHEP_B64;
-  var bin = Uint8Array.from(atob(b64), function (c) { return c.charCodeAt(0); });
-  var html = await new Response(new Blob([bin]).stream().pipeThrough(new DecompressionStream("gzip"))).text();
-  document.open(); document.write(html); document.close();
-})();
