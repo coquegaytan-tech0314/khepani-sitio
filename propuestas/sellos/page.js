ded1cfe9a6821f220f18add2e8187bcea599d68f
coquegaytan-tech0314 <coquegaytan@gmail.com>
2026-08-31 12:45:24 -0600

    Update sellos gzip loader index and page.js

--- propuestas/sellos/page.js
+++ propuestas/sellos/page.js
@@ -2,7 +2,5 @@
   var b64 = window.__KHEP_B64;
   var bin = Uint8Array.from(atob(b64), function (c) { return c.charCodeAt(0); });
   var html = await new Response(new Blob([bin]).stream().pipeThrough(new DecompressionStream("gzip"))).text();
-  document.open();
-  document.write(html);
-  document.close();
+  document.open(); document.write(html); document.close();
 })();
